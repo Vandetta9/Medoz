74f5002b89997595b4a1684860133f5a54a4968d
Vandetta9 <43728654+Vandetta9@users.noreply.github.com>
2018-11-02 23:10:19 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Hi ${client.user.tag} , This Code by : Kahrbaa `);
+  console.log('')
+  console.log(`i Have  [ " ${client.guilds.size} " ]`);
+});
+
+const developers = ["503701305764872212"]
+const adminprefix = "وخلو بالكو عشان الساحة اللي فرحانين بيها هبلعكو بيها !!";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+client.on('guildMemberAdd', member => {
+const Kahrba = member.guild.channels.get("503701305764872212");//ايدي الشات
+if(!Kahrba) return;
+if(Kahrba) {
+Kahrba.send(`**Welcome To overHype :heart: :revolving_hearts: **`);          
+}
+});
+
+
+
+client.on('message', message => {
+
+    if (message.content.startsWith( adminprefix + "avatar")) {
+        if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+        var mentionned = message.mentions.users.first();
+
+ if (mentionned) return;
+          var x5bzm = message.author;
+      
+        const embed = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.login(process.env.TOKEN);
